b1b02bf4d787961329e03dc3d0e00d0126e06b40
october <octoberserver@gmail.com>
2024-09-03 22:45:17 +0800

    .

---
+++ reversed-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#define Hello int
+#define World(x) main() {printf("Hello World");}
+
+
+Hello World("print")
---
+++ reversed.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#define Hello int
+#define World(x) main() {printf(x);}
+
+
+Hello World("print")
